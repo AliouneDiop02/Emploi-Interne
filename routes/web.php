03cd28a1b8aabc66c45e58d1b454fb45b75faad0
AliouneDiop02 <aliounediop0102@gmail.com>
2026-06-10 08:58:44 -0400

    feat: routes et contrôleurs crées

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,36 @@
 <?php
 
+use App\Http\Controllers\Admin\CandidatureOffreController;
+use App\Http\Controllers\Admin\CvController;
+use App\Http\Controllers\Admin\OffreEmploiController as AdminOffreEmploiController;
+use App\Http\Controllers\CandidatureController;
+use App\Http\Controllers\OffreEmploiController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::redirect('/', '/emplois');
+
+Route::get('/emplois', [OffreEmploiController::class, 'index'])
+    ->name('emplois.index');
+
+Route::get('/emplois/{offreEmploi}', [OffreEmploiController::class, 'show'])
+    ->name('emplois.show');
+
+Route::get('/emplois/{offreEmploi}/postuler', [CandidatureController::class, 'create'])
+    ->name('candidatures.create');
+
+Route::post('/emplois/{offreEmploi}/postuler', [CandidatureController::class, 'store'])
+    ->name('candidatures.store');
+
+
+Route::prefix('admin')->name('admin.')->group(function () {
+
+    Route::get('/offres/{offreEmploi}/candidatures', [CandidatureOffreController::class, 'index'])
+        ->name('offres.candidatures.index');
+
+    Route::resource('offres', AdminOffreEmploiController::class)
+        ->parameters(['offres' => 'offreEmploi'])
+        ->except(['show']);
+
+    Route::get('/candidatures/{candidature}/cv', [CvController::class, 'show'])
+        ->name('candidatures.cv');
 });
